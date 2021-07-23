f15e9da311710f801d28a0b66a4e33c5318b72f2
raphael-souza <raphael3130@gmail.com>
2021-07-23 08:51:51 -0300

    correção json no body

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,7 @@ import cors from 'cors';
 
 export const app = express()
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use('/user', router);
-app.use('/', (req, res) => res.send("API solid"));
-
-// export { app }
